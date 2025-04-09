20ae225810d813cb50466e7a8283870b63a46ee3
Plattner <rene.plattner.cs@gmail.com>
2025-04-09 13:48:49 +0200

    added offset to navigate leistungsnachweis

--- AutomatisierungRene/leistungsnachweis_navigation.py
+++ AutomatisierungRene/leistungsnachweis_navigation.py
@@ -3,19 +3,23 @@ from windia_enums import WindiaWindows
 from pywinauto import keyboard
 
 def input_hours_for_bill(automationManager : AutomationManager):
-
+    offset = 0.024
     automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS, 6/9 , 1/8)
 
     #Click inside Table
-    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS, 3/10 , 18/50)
+    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS, 3/10 , 18/50 + offset)
     keyboard.send_keys("200")
-    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS, 3/10 , 19/50)
+    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS, 3/10 , 19/50 + offset)
 
-    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS,3/10 , 20/50)
+    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS,3/10 , 20/50 + offset)
 
     keyboard.send_keys("200")
-    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS,3/10 , 21/50)
+    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS,3/10 , 21/50 + offset)
 
 
     #click Rechnug = Done
-    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS,3/11 , 9/10)
+    #automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS,3/11 , 9/10)
+
+a = AutomationManager()
+a.cur_selected_patient = "Plattner"
+input_hours_for_bill(a)
