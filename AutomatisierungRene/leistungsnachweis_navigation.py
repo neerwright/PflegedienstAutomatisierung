32903590db1c5335a3f601eb7c0819dbd4b22c2a
Plattner <rene.plattner.cs@gmail.com>
2025-04-09 14:30:49 +0200

    navigate leistungsnachweis no longer by relative ccords but by a distance from the top left corner

--- AutomatisierungRene/leistungsnachweis_navigation.py
+++ AutomatisierungRene/leistungsnachweis_navigation.py
@@ -1,25 +1,43 @@
 from automation_manager import AutomationManager
 from windia_enums import WindiaWindows
-from pywinauto import keyboard
+from pywinauto import keyboard, mouse
 
 def input_hours_for_bill(automationManager : AutomationManager):
-    offset = 0.024
+    offset = 16
+    x = 465
+    y = 320
+    #click Selbstzahler tab
     automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS, 6/9 , 1/8)
-
+    click_distance_from_top_left(465,320,a)
     #Click inside Table
-    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS, 3/10 , 18/50 + offset)
+    click_distance_from_top_left(x,y,a)
+    y += offset
     keyboard.send_keys("200")
-    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS, 3/10 , 19/50 + offset)
 
-    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS,3/10 , 20/50 + offset)
+    click_distance_from_top_left(x,y,a)
+    y += offset
+
+    click_distance_from_top_left(x,y,a)
+    y += offset
 
     keyboard.send_keys("200")
-    automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS,3/10 , 21/50 + offset)
+    click_distance_from_top_left(x,y,a)
+    y += offset
 
 
     #click Rechnug = Done
     #automationManager.click_inside_window(WindiaWindows.LEISTUNGS_NACHWEIS,3/11 , 9/10)
 
+def click_distance_from_top_left(x : int , y : int , automationManager : AutomationManager): 
+    rec = automationManager._get_rec(WindiaWindows.LEISTUNGS_NACHWEIS)
+
+    multiplier = 1 
+    if (rec.left < 40):
+        multiplier = 1.25 #open on small laptop display
+    #
+    mouse.click(coords= (int((int(rec.left) + int(x)) * multiplier), int((int(rec.top) + int(y)) *multiplier)))
+
 a = AutomationManager()
 a.cur_selected_patient = "Plattner"
+
 input_hours_for_bill(a)
